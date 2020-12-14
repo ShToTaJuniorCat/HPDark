490a1e1378759dfeaee429564f23e7772d7eced3
Peleg <peleghportal@gmail.com>
2020-12-15 00:47:09 +0200

    Updated TODOs, added comments and more.
    - Added a few files to make it easier for me to manage that I have left to do, what needs testing/fixing, and what is already done. Gosh, I have a TON of work left to do.
    - Added some comments on certain code blocks.

--- HPDark/script.js
+++ HPDark/script.js
@@ -1,4 +1,6 @@
 function getCookie(name) {
+    // Returns value of cookie named {name}. Function from stackoverflow
+
     var dc = document.cookie;
     var prefix = name + "=";
     var begin = dc.indexOf("; " + prefix);
@@ -15,27 +17,11 @@ function getCookie(name) {
         }
     }
     return decodeURI(dc.substring(begin + prefix.length, end));
-} 
-
-if(getCookie("darkMode") == null) {
-    document.cookie = "darkMode=1";
-    document.body.classList.add("darkModeOn");
-}
-
-if(getCookie("darkMode") == 1) {
-    document.body.classList.add("darkModeOn");
-}
-
-window.onload = function () {
-    var darkSwitch = document.createElement("div");
-    darkSwitch.setAttribute("class", "darkSwitch");
-    darkSwitch.setAttribute("id", "darkSwitch");
-    document.body.appendChild(darkSwitch);
-    console.log("Appended");
-    document.getElementById("darkSwitch").onclick = switchDarkMode;
 }
 
 function switchDarkMode() {
+    // Toggles the dark mode, on/off
+
     if(getCookie("darkMode") == 1) {
         document.cookie = "darkMode=0";
         document.body.classList.remove("darkModeOn");
@@ -45,4 +31,68 @@ function switchDarkMode() {
         document.cookie = "darkMode=1";
         document.body.classList.add("darkModeOn");
     }
+}
+
+/*
+Used to develop WYSIWYG
+
+function BBCodeToHTML($str) {
+    // Converts BBCode to HTML using regex. Function from stackoverflow
+
+    // The array of regex patterns to look for
+    $format_search =  [
+        /\[b\](.*?)\[\/b\]/ig,
+        /\[i\](.*?)\[\/i\]/ig,
+        /\[u\](.*?)\[\/u\]/ig
+    ]; // note: NO comma after the last entry
+
+    // The matching array of strings to replace matches with
+    $format_replace = [
+        '<strong>$1</strong>',
+        '<em>$1</em>',
+        '<span style="text-decoration: underline;">$1</span>'
+    ];
+
+    // Perform the actual conversion
+    for (var i =0;i<$format_search.length;i++) {
+      $str = $str.replace($format_search[i], $format_replace[i]);
+    }
+    return $str;
+}
+
+function showComment() {
+    
+
+    document.getElementById("finalComment").innerHTML = BBCodeToHTML(textarea.value);
+}
+*/
+
+// --------------------------------------------
+// By default, dark mode is turned on.
+if(getCookie("darkMode") == null) {
+    /* No cookie named "darkMode".
+    It probably means that this is the first time the extention is used on this device,
+    so... lets create that cookie!
+    */
+    document.cookie = "darkMode=1";
+    document.body.classList.add("darkModeOn");
+}
+
+if(getCookie("darkMode") == 1) {
+    // Dark mode is on, so lets turn it on, shall we?
+    document.body.classList.add("darkModeOn");
+}
+// --------------------------------------------
+
+window.onload = function () {
+    // --------------------------------------------
+    // Make a switch button to the dark mode
+    var darkSwitch = document.createElement("div");
+
+    darkSwitch.setAttribute("class", "darkSwitch");
+    darkSwitch.setAttribute("id", "darkSwitch");
+    document.body.appendChild(darkSwitch);
+    console.log("Dark Mode switch created");
+    document.getElementById("darkSwitch").onclick = switchDarkMode;
+    // --------------------------------------------
 }
