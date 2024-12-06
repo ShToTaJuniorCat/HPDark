c11bc3b7626ba05dce68c14cb9b26e4474346fea
Peleg <peleghportal@gmail.com>
2024-12-07 00:35:27 +0200

    Update chrome version
    - The chrome version is now up to date with the firefox one, yay!
    - This version is now submitted to the extension store of both browsers!
    - I really have to go to sleep now since I'm tired AF and need to wake up early!
    - bye

--- HPlus-Chrome/background.js
+++ HPlus-Chrome/background.js
@@ -1,1 +1,152 @@
+import { 
+    saveOwlToDB,
+    getAllOwls, 
+    getOwlByID, 
+    saveImportedOwls, 
+    deleteOwlByID, 
+    deleteAllSavedOwls,
+    deleteAllRealOwlsWithCopy, 
+    deleteRealOwl,
+    getIndexedDBUsage,
+    getSavedOwlCount
+} from './owlsDBFunctions.js';
+
+// Handle background messages 
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action == "save_owl" && message.owlID && message.bbcode && message.htmlContent) {
+        console.log("attempting to save owl " + message.owlID);
+        
+        saveOwlToDB(message.owlID, message.bbcode, message.htmlContent)
+            .then(response => {
+                console.log("Owl " + message.owlID + " saved successfully.");
+                sendResponse({ reply: response });
+            })
+            .catch(error => {
+                console.log("Error: " + error);
+                sendResponse({ error: error });
+            });
+
+        return true;
+    } else if (message.action == "get_all_owls") {
+        console.log("attempting to get all owls");
+
+        getAllOwls()
+            .then(response => {
+                console.log("Success!");
+                sendResponse({ reply: response });
+            })
+            .catch(error => {
+                console.log("Error: " + error);
+                sendResponse({ reply: error });
+            });
+
+        return true;
+    } else if (message.action == "get_owl" && message.owlID) {
+        console.log("attempting to retreive owl " + message.owlID);
+        getOwlByID(message.owlID)
+            .then(response => {
+                console.log("Owl " + message.owlID + " retreived successfully.");
+                sendResponse({ reply: response });
+            })
+            .catch(error => {
+                console.log("Error: " + error);
+                sendResponse({ reply: error });
+            });
+
+        return true;
+    } else if (message.action == "import_owls" && message.owls) {
+        console.log("attempting to import owls");
+        saveImportedOwls(message.owls)
+            .then(response => {
+                console.log(response);
+                sendResponse({ reply: response });
+            })
+            .catch(error => {
+                console.log("Error: " + error);
+                sendResponse({ error: error });
+            });
+
+        return true;
+    } else if (message.action == "delete_owl_from_db" && message.owlID) {
+        console.log("Attempting to delete owl " + message.owlID + " from DB");
+        deleteOwlByID(message.owlID)
+            .then(response => {
+                console.log(response);
+                sendResponse({ reply: response });
+            })
+            .catch(error => {
+                console.log("Error: " + error);
+                sendResponse({ reply: error });
+            });
+        
+        return true;
+    } else if (message.action == "delete_all_saved_owls") {
+        console.log("Attempting to delete owl all saved owls");
+        deleteAllSavedOwls()
+            .then(response => {
+                console.log(response);
+                sendResponse({ reply: response });
+            })
+            .catch(error => {
+                console.log("Error: " + error);
+                sendResponse({ reply: error });
+            });
+        
+        return true;
+    } else if (message.action == "delete_owl_with_copy") {
+        console.log("Attempting to delete owl all owls with saved copy");
+        deleteAllRealOwlsWithCopy()
+            .then(response => {
+                console.log(response);
+                sendResponse({ reply: response });
+            })
+            .catch(error => {
+                console.log("Error: " + error);
+                sendResponse({ reply: error });
+            });
+        
+        return true;
+    } else if (message.action == "delete_real_owl" && message.owlID) {
+        console.log("Attempting to delete owl " + message.owlID + " from hportal");
+        deleteRealOwl(message.owlID)
+            .then(response => {
+                console.log(response);
+                sendResponse({ reply: response });
+            })
+            .catch(error => {
+                console.log("Error: " + error);
+                sendResponse({ reply: error });
+            });
+        
+        return true;
+    } else if (message.action == "get_db_storage_size") {
+        console.log("Attempting to get IndexedDB storage size");
+        getIndexedDBUsage()
+            .then(response => {
+                console.log(response);
+                sendResponse({ reply: response });
+            })
+            .catch(error => {
+                console.log("Error: " + error);
+                sendResponse({ reply: error });
+            });
+        
+        return true;
+    } else if (message.action == "get_saved_owls_count") {
+        console.log("Attempting to saved owl count");
+        getSavedOwlCount()
+            .then(response => {
+                console.log(response);
+                sendResponse({ reply: response });
+            })
+            .catch(error => {
+                console.log("Error: " + error);
+                sendResponse({ reply: error });
+            });
+        
+        return true;
+    }
+});
+
+// Set this url to open upon uninstall
 chrome.runtime.setUninstallURL("https://forms.gle/r2afqHqEr3P7cXoF7");
